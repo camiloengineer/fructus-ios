c79570ed4d0a97c91781e7ac31e83363d1c8c8df
Camilo González <cgonzalezm1234@gmail.com>
2022-07-23 12:25:38 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Camilo Gonzalez on 23-07-22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
